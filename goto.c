dd5fb72cb4f82fd9f2cb81be68f53790164b6fec
ENDcodeworld <121748084+ENDcodeworld@users.noreply.github.com>
2023-06-22 08:35:12 +0800

    Add files via upload

---
+++ goto.c
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+//int main()
+//{
+//	char input[20] = { 0 };
+//	system("shutdown -s -t 120000");
+//again:
+//	printf("你的电脑在120内要关机了,如果想取消,请输入:鸡你太美\n");
+//	scanf("%s", input);
+//	if (strcmp(input, "鸡你太") == 0)
+//	{
+//		system("shutdown - a");
+//		break;
+//	}
+//	else 
+//	{
+//		goto again;
+//	}
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char input[20] = { 0 };
+//
+//	system("shutdown -s -t 300");
+//again:
+//	printf("系统将要关机，如果不想就输入:我无情\n");
+//	scanf("%s", input);
+//	if (strcmp(input, "我无情") == 0)
+//	{
+//		system("shutdown -a");
+//		
+//	}
+//	else
+//	{
+//		goto again;
+//	}
+//
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	char input[20] = { 0 };
+//    system("shutdown - s - t 120");
+//	
+//	while (1)
+//	{
+//		
+//	    printf("系统将要关机，请输入：不关只因\n");
+//	    scanf("%s", input);
+//		
+//		if (strcmp(input, "不关只因") == 0)
+//		{
+//			system("shutdown -a");
+//			break;
+//		}
+//	}
+//	
+//
+//	return 0;
+//}
+
